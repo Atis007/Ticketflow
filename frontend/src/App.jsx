bc7b3484a1a893ab61d58bc5b9bf842098a1da8f
Tóth Attila <tothattila5559@gmail.com>
2025-12-10 20:22:23 +0100

    Cleanup repository: remove previously tracked files and apply updated .gitignore rules

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="p-10 bg-blue-500 text-white text-3xl font-bold">
-      Tailwind (Vite plugin) is working!
-    </div>
-  );
-}
-
-export default App;
